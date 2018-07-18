d676f9ce4e94aab7ba35ad93c9a1e892eede93c0
topepo <mxkuhn@gmail.com>
2018-07-18 11:28:01 -0400

    changes based on ideas from HW

--- R/tags.R
+++ R/tags.R
@@ -10,23 +10,23 @@ tags <- list(
 
 
 #' Facilities for loading and updating other packages
-#' 
-#' `tidymodels` contains a set of tags for specific topics in 
+#'
+#' `tidymodels` contains a set of tags for specific topics in
 #'  modeling and analysis. These can be used to load packages
-#'  in groups. 
-#'  
+#'  in groups.
+#'
 #' @param tag A character strong for the tag. Current options
-#'  are 'Bayesian analysis', 'deep learning', 'feature engineering', 
-#'  'parallel processing', 'resampling', 'text analysis',  and 
+#'  are 'Bayesian analysis', 'deep learning', 'feature engineering',
+#'  'parallel processing', 'resampling', 'text analysis',  and
 #'  'time series'.
-#'  
-#' @examples 
+#'
+#' @examples
 #' tag_show()
 #' @aliases tags
 #' @export
 tag_show <- function() {
   all_tags <- vapply(
-    tags, 
+    tags,
     function(x) quote_pkg(x),
     character(1))
   all_tags <- paste0(names(all_tags), ": ", all_tags, "\n")
@@ -39,10 +39,7 @@ tag_show <- function() {
 #' @export
 #' @rdname tag_show
 tag_attach <- function(tag) {
-  if (!is.character(tag) || length(tag) != 1)
-    stop("`tag` should be one of: ",
-         paste0("'", names(tags), "'", collapse = ", "),
-         call. = FALSE)
+  tag_validate(tag)
   pkgs <- unlist(tags[[tag]])
   installed <- rownames(installed.packages())
   is_installed <- pkgs %in% installed
@@ -51,21 +48,23 @@ tag_attach <- function(tag) {
          quote_pkg(pkgs[!is_installed]),
          call. = FALSE)
   }
-  
   tidymodels_attach(unique(pkgs))
 }
 
 #' @export
 #' @rdname tag_show
 tag_update <- function(tag) {
-  if (!is.character(tag) || length(tag) != 1)
-    stop("`tag` should be one of: ",
-         paste0("'", names(tags), "'", collapse = ", "),
-         call. = FALSE)
-  
+  tag_validate(tag)
   pkgs <- tags[[tag]]
   tidymodels_update(unique(unlist(pkgs)))
 }
 
-quote_pkg <- function(x) 
+quote_pkg <- function(x)
   paste0("'", x, "'", collapse = ", ")
+
+tag_validate <- function(tag) {
+  if (!is.character(tag) || length(tag) != 1)
+    stop("`tag` should be one of: ",
+         paste0("'", names(tags), "'", collapse = ", "),
+         call. = FALSE)
+}
