37b48451814c3a77d381e19ac0a172a75e85f10e
topepo <mxkuhn@gmail.com>
2018-11-19 21:29:46 -0500

    remove R CMD check warnings

---
+++ R/imports.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+# Suppress R CMD check note
+#' @importFrom infer chisq_stat
+#' @importFrom pillar colonnade
+#' @importFrom ggplot2 ggplot
+#' @importFrom recipes prep
+#' @importFrom rsample analysis
+#' @importFrom tidyposterior Fisher_trans
+#' @importFrom tidypredict acceptable_formula
+#' @importFrom tidytext get_sentiments
+#' @importFrom yardstick accuracy
+NULL
